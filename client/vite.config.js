e8efb1ae5dd892a661c19d5e9638203fb0f378d3
Nesha Mervin <nesha.mervin@gmail.com>
2024-08-12 15:53:46 -0400

    initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8000/',
+    },
+  },
+});
